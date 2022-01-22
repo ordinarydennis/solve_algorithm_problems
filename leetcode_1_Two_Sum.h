b168aac5956bb7c1364eeab396a3bb1f0d1deb6e
ordinarydennis <ordinarydennis@gmail.com>
2022-01-22 22:41:58 +0900

    Edit Leet Code Solution 1. Two Sum. - Added solution solved by two  pass hash map.

--- leetcode_1_Two_Sum.h
+++ leetcode_1_Two_Sum.h
@@ -1,3 +1,6 @@
+
+
+//Brute force
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -29,4 +32,29 @@ public:
         return std::move(ret);
         
     }
+};
+
+//Two-pass Hash Table
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+
+		map<int, int> map;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			map.emplace(nums[i], i);
+		}
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			int remain = target - nums[i];
+			auto it = map.find(remain);
+			if (it != map.end())
+			{
+				return vector<int>{it->second, i};
+			}
+		}
+
+	}
 };
