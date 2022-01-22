b6784bb96713bc381e782aa30a404c20d4d95878
ordinarydennis <ordinarydennis@gmail.com>
2022-01-22 22:49:08 +0900

    Edit Leet Code Solution 1. Two Sum - Edit map -> unordered_map

--- leetcode_1_Two_Sum.h
+++ leetcode_1_Two_Sum.h
@@ -39,7 +39,7 @@ class Solution {
 public:
 	vector<int> twoSum(vector<int>& nums, int target) {
 
-		map<int, int> map;
+		unordered_map<int, int> map;
 
 		for (int i = 0; i < nums.size(); i++)
 		{
@@ -52,9 +52,14 @@ public:
 			auto it = map.find(remain);
 			if (it != map.end())
 			{
-				return vector<int>{it->second, i};
+				if (it->second != i)
+				{
+					return vector<int>{it->second, i};
+				}
 			}
 		}
+
+		return vector<int>();
 
 	}
 };
