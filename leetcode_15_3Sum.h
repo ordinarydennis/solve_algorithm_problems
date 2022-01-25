280f0520ddaba2e960876a05e4fc894753d16d09
오준영 <ordinarydennis@gmail.com>
2022-01-25 14:13:05 +0900

    Add leet code solution 15. 3Sum

--- leetcode_15_3Sum.h
+++ leetcode_15_3Sum.h
@@ -8,50 +8,41 @@ public:
 
 		for (int i = 0; i < nums.size(); i++)
 		{
-			int pivot = nums[i];
+			if (i != 0 && nums[i - 1] == nums[i])
+			{
+				continue;
+			}
+
 			int l = i + 1;
 			int r = nums.size() - 1;
-			int target = pivot * -1;
 
 			while (l < r)
 			{
-				int sum = nums[l] + nums[r];
+				int sum = nums[i] + nums[l] + nums[r];
 
-				if (sum == target)
+				if (sum == 0)
 				{
-					ret.push_back(vector<int>{target, nums[l] , nums[r]});
+					ret.push_back(
+						std::move(vector<int>{nums[i], nums[l], nums[r]})
+					);
+
 					l++;
 					r--;
 
-					while (l < r)
-					{
-						if (nums[l] == nums[l - 1]) l++;
-						if (nums[r] == nums[r - 1]) r--;
-					}
+					while (l < r && nums[l] == nums[l - 1]) l++;
+					while (l < r && nums[r] == nums[r + 1]) r--;
 				}
-				else if (target < sum)
+				else if (0 < sum)
 				{
 					r--;
 				}
 				else
 				{
-					l--;
+					l++;
 				}
-
-
-
 			}
 
-
-
-
 		}
-
-
-
-
-
-
 
 		return ret;
 
