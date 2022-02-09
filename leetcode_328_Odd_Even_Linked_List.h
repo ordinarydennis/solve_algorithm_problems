173b8acb7c908040b48042f9eff6cc52bb049e8c
오준영 <ordinarydennis@gmail.com>
2022-02-09 14:18:08 +0900

    Edit leet code solution - 328. Odd Even Linked List(fail)

--- leetcode_328_Odd_Even_Linked_List.h
+++ leetcode_328_Odd_Even_Linked_List.h
@@ -1,12 +1,3 @@
-//Given the head of a singly linked list, group all the nodes with odd indices together 
-// followed by the nodes with even indices, and return the reordered list.
-//
-//The first node is considered odd, and the second node is even, and so on.
-//
-//Note that the relative order inside both the evenand odd groups should remain as it was in the input.
-//
-//You must solve the problem in O(1) extra space complexityand O(n) time complexity.
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -17,11 +8,6 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-//Input: head = [2, 1, 3, 5, 6, 4, 7]
-//Output : [2, 3, 6, 7, 1, 5, 4]
-
-//[1,2,3,4,5]
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
@@ -30,11 +16,12 @@ public:
         ListNode* evenNodeIt = nullptr;
         ListNode* evenNodeHead = nullptr;
         ListNode* oddNodeLast = nullptr;
-        //짝수 노드를 가져오면서 홀수 노드끼리 연결해야 한다.
+
         while (nodeIt)
         {
             if (nullptr == nodeIt->next)
             {
+                oddNodeLast = nodeIt;
                 break;
             }
 
@@ -45,34 +32,17 @@ public:
             }
             else
             {
-                //2 > 4
                 evenNodeIt->next = nodeIt->next;
                 evenNodeIt = nodeIt->next;
             }
 
-            //1 > 3 연결
-            //3 > 5 연결
-            if (evenNodeIt)
-            {
-                nodeIt->next = evenNodeIt->next;
+			nodeIt->next = evenNodeIt->next;
 
-				//3을 1번째로 잡는다.
-				if (nullptr != nodeIt->next)
-				{
-                    nodeIt = nodeIt->next;
-				}
-                else
-                {
-					oddNodeLast = nodeIt;
-                    nodeIt = nullptr;
-					break;
-                }
-            }
+            nodeIt = nodeIt->next;
         }
 
         oddNodeLast->next = evenNodeHead;
 
         return head;
-
     }
 };
