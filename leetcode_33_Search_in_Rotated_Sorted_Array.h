e262a121d677d450211cae4e0abd86b21381e5bf
오준영 <ordinarydennis@gmail.com>
2022-02-21 14:15:00 +0900

    Edit LeetCode Solution - 33. Search in Rotated Sorted Array - It is more efficient but not correct answer.

--- leetcode_33_Search_in_Rotated_Sorted_Array.h
+++ leetcode_33_Search_in_Rotated_Sorted_Array.h
@@ -3,42 +3,13 @@ public:
 	int search(vector<int>& nums, int target) {
 
 		int ret = -1;
-		int zeroIndex = -1;
 
 		for (int i = 0; i < nums.size(); i++)
 		{
 			if (target == nums[i])
 			{
 				ret = i;
-				if (zeroIndex != -1)
-				{
-					break;
-				}
-			}
-
-			if (nums[i] == 0)
-			{
-				zeroIndex = i;
-				if (-1 != ret)
-				{
-					break;
-				}
-			}
-		}
-
-		for (int i = 0; i < zeroIndex - 1; i++)
-		{
-			if (nums[i] >= nums[i+1])
-			{
-				return -1;
-			}
-		}
-
-		for (int i = zeroIndex; i < nums.size() -1; i++)
-		{
-			if (nums[i] >= nums[i + 1])
-			{
-				return -1;
+				break;
 			}
 		}
 
