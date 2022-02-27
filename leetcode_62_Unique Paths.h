b648ca120305b77d1c1a64fbe845c8dc088dc8c0
Dennis <ordinarydennis@gmail.com>
2022-02-27 22:55:04 +0900

    Edit LeetCode Solution - 62. Unique Paths

--- leetcode_62_Unique Paths.h
+++ leetcode_62_Unique Paths.h
@@ -1,38 +1,44 @@
+//Permutations with Repetition
+//class Solution {
+//
+//    double factorial(double n)
+//    {
+//        double result = 1;
+//        for (int i = 1; i <= n; i++)
+//            result *= i;
+//        return result;
+//    }
+//
+//public:
+//    int uniquePaths(int m, int n) {
+//
+//        m--;
+//        n--;
+//
+//        return round(factorial(m + n) / (factorial(m) * factorial(n)));
+//
+//    }
+//};
+
+//The number of shortest distance cases.
+//Time complexity : O(m * n)
+//Space comlexity : O(m * n)
 class Solution {
-
-    int maxM = 0;
-    int maxN = 0;
-
-    int ret = 0;
-
-    void dfs(int m, int n)
-    {
-        if (maxM <= m || maxN <= n)
-        {
-            return;
-        }
-
-        if (maxM - 1 == m && maxN - 1 == n)
-        {
-            ret++;
-            return;
-        }
-
-        dfs(m, n + 1);
-
-        dfs(m + 1, n);
-
-    }
 
 public:
     int uniquePaths(int m, int n) {
 
-        maxM = m;
-        maxN = n;
+        vector<vector<int>> dp(m, vector<int>(n, 1));
 
-        dfs(0, 0);
+        for (int a =1; a < m; a++)
+        {
+            for (int b = 1; b < n; b++)
+            {
+                dp[a][b] = dp[a - 1][b] + dp[a][b - 1];
+            }
+        }
 
-        return ret;
+        return dp[m - 1][n - 1];
 
     }
 };
