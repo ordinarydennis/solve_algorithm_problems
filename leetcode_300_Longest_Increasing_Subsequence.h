9698020157b853848ff63395f0b3d74136be4ab1
ordinarydennis <ordinarydennis@gmail.com>
2022-03-05 20:33:20 +0900

    Edit Leetcode Solution - 300. Longest Increasing Subsequence

--- leetcode_300_Longest_Increasing_Subsequence.h
+++ leetcode_300_Longest_Increasing_Subsequence.h
@@ -1,58 +1,21 @@
-
 class Solution {
 public:
-	int lengthOfLIS(vector<int>& nums) {
+    int lengthOfLIS(vector<int>& nums) {
 
-		vector<int> dp(nums.size(), 0);
+        vector<int> dp(nums.size(), 1);
 
-		dp[nums.size() - 1] = 1;
+        for (int a = 1; a < nums.size(); a++)
+        {
+            for (int b = 0; b < a; b++)
+            {
+                if (nums[b] < nums[a] && dp[a] < dp[b] + 1)
+                {
+                    dp[a] = dp[b] + 1;
+                }
+            }
+        }
+        
+        return *std::max_element(dp.begin(), dp.end());
 
-		for (int i = nums.size() - 2; i >= 0; i--)
-		{
-			if (nums[i] < nums[i + 1])
-			{
-				if (0 != dp[i + 1])
-				{
-					dp[i] = dp[i + 1] + 1;
-				}
-				else
-				{
-					for (int a = i + 2; a < nums.size(); a++)
-					{
-						if (nums[i] < nums[a])
-						{
-							dp[i] = dp[a] + 1;
-							break;
-						}
-						else if (nums[i] == nums[a])
-						{
-							dp[i] = dp[a];
-							break;
-						}
-					}
-				}
-			}
-			else
-			{
-				for (int a = i + 2; a < nums.size(); a++)
-				{
-					if (nums[i] < nums[a])
-					{
-						dp[i] = dp[a] + 1;
-						break;
-					}
-					else if (nums[i] == nums[a])
-					{
-						dp[i] = dp[a];
-						break;
-					}
-				}
-			}
-		}
-
-		sort(dp.begin(), dp.end());
-
-		return dp[nums.size() - 1];
-
-	}
+    }
 };
