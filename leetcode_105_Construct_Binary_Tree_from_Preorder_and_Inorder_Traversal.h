95b99f3e0757162840a0040e83644ef1ed4206f1
오준영 <ordinarydennis@gmail.com>
2022-03-07 21:01:00 +0900

    Add LeetCode Solution - 105. Construct Binary Tree from Preorder and Inorder Traversal(fail)

---
+++ leetcode_105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.h
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        
+        //프리 오더의 첫 인자는 루트 노드다.
+        //루트 노드를 인오더에서 찾는다.
+        //루트 노드와 인오더의 루트 앞에 노드와 같다면 자식이다. 
+
+
+
+    }
+};
