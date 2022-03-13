41d9d70c97800aee5c6682a7eb7a62d709683da4
ordinarydennis <ordinarydennis@gmail.com>
2022-03-13 20:19:45 +0900

    Edit LeetCode Solution - 95. Unique Binary Search Trees II (correct answer) - Need to study again.

--- leetcode_95_Unique_Binary_Search_Trees_II.h
+++ leetcode_95_Unique_Binary_Search_Trees_II.h
@@ -9,67 +9,103 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //Divide-and-conquer.
 class Solution {
-
-    vector<TreeNode*> ret;
-
-    int max = 0; 
-
-    void dfs(int n, TreeNode* root, int c)
+public:
+    vector<TreeNode*> generateTrees(int start, int end)
     {
-        if (max <= c)
+        vector<TreeNode*> binarySearchTrees;
+        if (start > end)
         {
-            return;
+            binarySearchTrees.push_back(NULL);
+            return binarySearchTrees;
         }
+        if (start == end)
+        {
+            binarySearchTrees.push_back(new TreeNode(start));
+            return binarySearchTrees;
+        }
+
+        for (int i = start; i <= end; i++)
+        {
+            vector<TreeNode*> leftList = generateTrees(start, i - 1);
+            vector<TreeNode*> rightList = generateTrees(i + 1, end);
+
+            for (int l = 0; l < leftList.size(); l++)
+            {
+                for (int r = 0; r < rightList.size(); r++)
+                {
+                    TreeNode* root = new TreeNode(i);
+                    root->left = leftList[l];
+                    root->right = rightList[r];
+
+                    binarySearchTrees.push_back(root);
+                }
+            }
+        }
+
+        return binarySearchTrees;
+    }
+
+    vector<TreeNode*> generateTrees(int n) {
+        return generateTrees(1, n);
+    }
+};
+
+//DP
+class Solution2 {
+public:
+    TreeNode* clone(TreeNode* root)
+    {
+        if (root == nullptr)
+            return nullptr;
+
+        TreeNode* newroot = new TreeNode(root->val);
+        newroot->left = clone(root->left);
+        newroot->right = clone(root->right);
+
+        return newroot;
+    }
+    vector<TreeNode*> generateTrees(int n)
+    {
+        vector<TreeNode*> res(1, nullptr);
 
         for (int i = 1; i <= n; i++)
         {
-            if (root->val == i)
+            vector<TreeNode*> tmp;
+            for (int j = 0; j < res.size(); j++)
             {
-                continue;
+                TreeNode* oldroot = res[j];
+                TreeNode* root = new TreeNode(i);
+                TreeNode* target = clone(oldroot);
+
+                root->left = target;
+
+                tmp.push_back(root);
+
+                if (oldroot != nullptr)
+                {
+                    TreeNode* tmpold = oldroot;
+                    while (tmpold->right != nullptr)
+                    {
+                        TreeNode* nonroot = new TreeNode(i);
+                        TreeNode* tright = tmpold->right;
+                        tmpold->right = nonroot;
+                        nonroot->left = tright;
+                        TreeNode* target = clone(oldroot);
+                        tmp.push_back(target);
+                        tmpold->right = tright;
+                        tmpold = tmpold->right;
+                    }
+
+                    tmpold->right = new TreeNode(i);
+                    TreeNode* target = clone(oldroot);
+                    tmp.push_back(target);
+                    tmpold->right = nullptr;
+                }
             }
-
-            if (root->val < i)
-            {
-               auto* node = new TreeNode;
-
-               node->val = i;
-
-               root->right = node;
-
-               dfs(n, node, c + 1);
-
-               ret.push_back(root);
-            }
-            else if(i < root->val)
-            {
-                auto* node = new TreeNode;
-
-                node->val = i;
-
-                root->left = node;
-
-                dfs(n, node, c + 1);
-
-                ret.push_back(root);
-            }
+            res = tmp;
         }
-
-    }
-
-
-
-public:
-    vector<TreeNode*> generateTrees(int n) {
-
-        max = n;
-
-        TreeNode* root = new TreeNode;
-
-        root->val = 1;
-
-        dfs(0, root, 1);
-
-        return ret;
+        return res;
     }
 };
