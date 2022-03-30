ae02236e692c0b7046edaf59339986544e0e4c6c
Dennis <ordinarydennis@gmail.com>
2022-03-30 20:05:35 +0900

    Add LeetCode Solution - 129. Sum Root to Leaf Numbers(4m)

---
+++ leetcode_129_Sum_Root_to_Leaf_Numbers.h
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+
+    int sum = 0;
+
+    void treeTraversal(TreeNode* root, int num)
+    {
+        int cur = (num * 10) + root->val;
+
+        if (nullptr == root->left && nullptr root->right)
+        {
+            sum += cur;
+        }
+
+
+        if (root->left)
+        {
+            treeTraversal(root->left, cur);
+        }
+
+        if (root->right)
+        {
+            treeTraversal(root->right, cur);
+        }
+    }
+
+public:
+    int sumNumbers(TreeNode* root) {
+
+        treeTraversal(root, 0);
+
+        return sum;
+    }
+};
