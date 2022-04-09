eaf59f39c10313b7576caf639d762ce0443fd600
Dennis <ordinarydennis@gmail.com>
2022-04-10 08:46:32 +0900

    Edit LeetCode Solution - 64. Minimum Path Sum(24m) - Edit with correct answerit

--- leetcode_64_Minimum_Path_Sum.h
+++ leetcode_64_Minimum_Path_Sum.h
@@ -1,5 +1,7 @@
 class Solution {
 
+	vector<vector<int>> dp;
+
 	int sumPathValue(vector<vector<int>>& grid, int x, int y)
 	{
 		if (grid.size() <= y || grid[0].size() <= x)
@@ -7,25 +9,33 @@ class Solution {
 			return INT_MAX;
 		}
 
-		int left = sumPathValue(grid, x + 1, y);
+		if (-1 != dp[y][x])
+		{
+			return dp[y][x];
+		}
 
-		int bottom = sumPathValue(grid, x, y + 1);
+		auto left = sumPathValue(grid, x + 1, y);
+
+		auto bottom = sumPathValue(grid, x, y + 1);
 
 		if (INT_MAX == left && INT_MAX == bottom)
 		{
-			return grid[y][x];
+			dp[y][x] = grid[y][x];
+			return dp[y][x];
 		}
 		else
 		{
-			grid[y][x] += std::min(left, bottom);
+			dp[y][x] = grid[y][x] + std::min(left, bottom);
 		}
 
-		return grid[y][x];
+		return dp[y][x];
 	}
 
 
 public:
 	int minPathSum(vector<vector<int>>& grid) {
+
+		dp.resize(grid.size(), vector<int>(grid[0].size(), -1));
 
 		return sumPathValue(grid, 0, 0);
 
