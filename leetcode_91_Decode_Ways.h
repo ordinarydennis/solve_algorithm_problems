70cc3207b28334cd56d0ef9c43acf544b95b4eb1
ordinarydennis <ordinarydennis@gmail.com>
2022-04-15 22:47:01 +0900

    Edit LeetCode Solution - 91. Decode Ways(read solution)

--- leetcode_91_Decode_Ways.h
+++ leetcode_91_Decode_Ways.h
@@ -1,38 +1,43 @@
 class Solution {
 
-	vector<string> list;
-
-	void dfs(string& s, string str, int index)
-	{
-		for (int i = index; i < s.size(); i++)
-		{
-			if ('0' == s[i])
-			{
-				continue;
-			}
-
-			str += s[i];
-
-			dfs(s, str, i + 1);
-
-			if (1 < str.size())
-			{
-				list.push_back(str);
-			}
-		}
-	}
-
+    std::vector<int> dp;
 
 public:
-	int numDecodings(string s) {
+    int numDecodings(const string& s)
+    {
+        dp.resize(s.size() + 1, 0);
 
-		list.reserve(1000);
+        if ('0' == s[0])
+        {
+            return 0;
+        }
 
-		string str;
+        dp[0] = 1;
+        dp[1] = 1;
 
-		dfs(s, str, 0);
+        for (int i = 2; i <= s.size(); i++)
+        {
+            if (s[i - 1] != '0')
+            {
+                dp[i] = dp[i - 1];
+            }
+            else
+            {
+                if (s[i - 2] != '1' && s[i - 2] != '2')
+                    return 0;
+            }
+            
+            if (s[i - 2] == '1')
+            {
+                dp[i] += dp[i - 2];
+            }
+            else if (s[i - 2] == '2' && s[i - 1] <= '6')
+            {
+                dp[i] += dp[i - 2];
+            }
 
-		return list.size();
+        }
 
-	}
+        return dp[s.length()];
+    }
 };
