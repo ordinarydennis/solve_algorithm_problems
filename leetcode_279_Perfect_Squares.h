ca49ba686790bba9520535fbdccdf14a4b69d68b
Dennis <ordinarydennis@gmail.com>
2022-05-07 09:25:19 +0900

    Edit LeetCode Solution - 279. Perfect Squares(another solution)

--- leetcode_279_Perfect_Squares.h
+++ leetcode_279_Perfect_Squares.h
@@ -1,53 +1,77 @@
 class Solution {
 
-    int min = INT_MAX;
+public:
+	int numSquares(int n) {
 
-    bool dfs(int n, int count, int remain)
-    {
-        if (remain < 0)
-        {
-            return false;
-        }
+		vector<int> dp(n + 1, INT_MAX);
 
-        if (min < count)
-        {
-            return true;
-        }
+		dp[0] = 0;
 
-        if (0 == remain)
-        {
-            min = std::min(min, count);
-            return true;
-        }
-
-
-        for (int i = n; 1 <= i; i--)
-        {
-            if (remain - (i * i) < 0)
-            {
-                continue;
-            }
-
-            bool result = dfs(i, count + 1, remain - (i * i));
-
-            if (result)
-            {
-                break;
-            }
-
-        }
+		for (int a = 1; a <= n; a++)
+		{
+			for (int b = 1; b * b <= a; b++)
+			{
+				dp[a] = std::min(
+					dp[a],
+					dp[a - b * b] + 1
+				);
+			}
+		}
+		return dp.back();
+	}
+};
 
 
-        return false;
-    }
+class Solution {
+
+	int min = INT_MAX;
+
+	bool dfs(int n, int count, int remain)
+	{
+		if (remain < 0)
+		{
+			return false;
+		}
+
+		if (min < count)
+		{
+			return true;
+		}
+
+		if (0 == remain)
+		{
+			min = std::min(min, count);
+			return true;
+		}
+
+
+		for (int i = n; 1 <= i; i--)
+		{
+			if (remain - (i * i) < 0)
+			{
+				continue;
+			}
+
+			bool result = dfs(i, count + 1, remain - (i * i));
+
+			if (result)
+			{
+				break;
+			}
+
+		}
+
+
+		return false;
+	}
 
 
 public:
-    int numSquares(int n) {
+	int numSquares(int n) {
 
-        dfs(n, 0, n);
+		dfs(n, 0, n);
 
-        return min;
+		return min;
 
-    }
+	}
 };
