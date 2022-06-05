b8ae060c859c26f1855604e2e2fbf0f719631a31
Dennis <ordinarydennis@gmail.com>
2022-06-05 16:05:49 +0900

    Edit LeetCode Solution - 494. Target Sum(resolve) - Solve as other solutions

--- leetcode_494_Target_Sum.h
+++ leetcode_494_Target_Sum.h
@@ -26,3 +26,79 @@ public:
         return count;
     }
 };
+
+
+class Solution {
+
+    int count = 0;
+
+    int find(int p, vector<int>& nums, int sum, vector<unordered_map<int, int>>& mem) {
+
+        if (p == nums.size()) return sum == 0;
+
+        auto it = mem[p].find(sum);
+
+        if (it != mem[p].end()) return it->second;
+
+        return mem[p][sum] = find(p + 1, nums, sum + nums[p], mem) + find(p + 1, nums, sum - nums[p], mem);
+
+    }
+
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+
+        vector<unordered_map<int, int>> mem(nums.size());
+
+        return find(0, nums, target, mem);
+    }
+};
+
+
+-1 + 1 + 1 + 1 + 1 = 3
++ 1 - 1 + 1 + 1 + 1 = 3
++ 1 + 1 - 1 + 1 + 1 = 3
++ 1 + 1 + 1 - 1 + 1 = 3
++ 1 + 1 + 1 + 1 - 1 = 3
+
++ 1 + 1 + 1 + 1 - 1 = 3
++ 1 + 1 + 1 + 1 + 1 = 4
+
++ 1 + 1 + 1 + 1 + 1 = 3
++ 1 + 1 + 1 + 1 - 1 = 4
+
+p : 4
+sum : 5
+mem[4][5] =  0
+
+p : 4
+sum : 3
+mem[4][3] = 0
+
+p : 3
+sum : 4
+mem[4][4] = 0
+
+p : 3
+sum : 3
+mem[4][3] = 0
+
+int findTargetSumWays(vector<int>& nums, int S) {
+    vector<unordered_map<int, int>> mem(nums.size());
+    return find(0, nums, S, mem);
+}
+
+
+int find(int p, vector<int>& nums, int sum, vector<unordered_map<int, int>>& mem) {
+
+    if (p == nums.size()) return sum == 0;
+    
+    auto it = mem[p].find(sum);
+    
+    if (it != mem[p].end()) return it->second;
+    
+    return mem[p][sum] = find(p + 1, nums, sum + nums[p], mem) + find(p + 1, nums, sum - nums[p], mem);
+
+}
+
++ 1 + 1 + 1 + 1 + 1 = 5
++ 1 + 1 + 1 + 1 - 1 = 3
