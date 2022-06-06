d3526865559a2bd20dda31ab5f528d05131a4dc8
Dennis <ordinarydennis@gmail.com>
2022-06-06 23:33:17 +0900

    Edit LeetCode Solution - 474. Ones and Zeroes(resolve)

--- leetcode_474_Ones_and_Zeroes.h
+++ leetcode_474_Ones_and_Zeroes.h
@@ -1,60 +1,22 @@
 class Solution {
 
-    vector<int> dpZero;
-    vector<int> dpOne;
-
-    int ret = 0;
-
-    void dfs(int pos, int m, int n, int sum0, int sum1, int count)
-    {
-        if (sum0 == m && sum1 == n)
-        {
-            ret = std::max(ret, count);
-            return;
-        }
-
-        if (m < sum0 || n < sum1)
-        {
-            return;
-        }
-
-        for (int i = pos; i < dpZero.size(); i++)
-        {
-            dfs(i + 1, m, n, dpZero[i] + sum0, dpOne[i] + sum1, count + 1);
-        }
-
-    }
-
 
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
 
-        dpZero.resize(strs.size(), 0);
-        dpOne.resize(strs.size(), 0);
+        vector<vector<int> > dp(m + 1, vector<int>(n + 1));
 
-        for (int i = 0; i < strs.size(); i++)
+        for (auto& str : strs)
         {
-            for (int a = 0; a < strs[i].size(); a++)
+            int zeros = count(begin(str), end(str), '0'), ones = size(str) - zeros;
+
+            for (int i = m; i >= zeros; i--)
             {
-
-                if ('1' == strs[i][a])
-                {
-                    dpOne[i]++;
-                }
-                else
-                {
-                    dpZero[i]++;
-                }
-
+                for (int j = n; j >= ones; j--)
+                    dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1);
             }
+
         }
-
-
-
-        dfs(0, m, n, 0, 0, 0);
-
-        return ret;
-
-
+        return dp[m][n];
     }
 };
