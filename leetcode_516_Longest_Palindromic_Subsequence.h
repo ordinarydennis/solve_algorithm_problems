399aed011ddf53d20e3638d8ee51191a64c4ca6f
ordinarydennis <ordinarydennis@gmail.com>
2022-06-13 21:27:35 +0900

    Add LeetCode Solution - 516. Longest Palindromic Subsequence(fail)

---
+++ leetcode_516_Longest_Palindromic_Subsequence.h
@@ -0,0 +1,28 @@
+class Solution {
+
+public:
+    int longestPalindromeSubseq(string s) {
+
+        int count = 0;
+
+        for (int a = 0; a < s.size() - 2; a++s)
+        {
+            for (int b = a + 1; b < s.size() - 1; b++)
+            {
+                if (s[a] == s[b])
+                {
+
+                }
+
+
+
+
+            }
+        }
+
+
+
+
+
+    }
+};
