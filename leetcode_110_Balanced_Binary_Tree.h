200c763070ebdc20b1e74c98389ad1ac3b3ab6e4
Dennis <ordinarydennis@gmail.com>
2022-07-02 09:11:49 +0900

    Edit LeetCode Solution - 110. Balanced Binary Tree(11m)

--- leetcode_110_Balanced_Binary_Tree.h
+++ leetcode_110_Balanced_Binary_Tree.h
@@ -9,52 +9,29 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-[1, null, 2, null, 3]
 class Solution {
 
 	int ret = true;
-	TreeNode* root = nullptr;
 
-	void traversal(TreeNode* node)
+	int traversal(TreeNode* node)
 	{
+
+
 		if (nullptr == node)
 		{
-			return;
+			return 1;
 		}
 
-		if (nullptr == node->left && nullptr == node->right)
+		int left = traversal(node->left) + 1;
+		int right = traversal(node->right) + 1;
+
+		if(1 < std::abs(left - right))
 		{
-			return;
+
+			ret = false;
 		}
 
-		if (root != node)
-		{
-			if (node->left && node->left->left)
-			{
-				ret = false;
-				return;
-			}
-			if (node->left && node->left->right)
-			{
-				ret = false;
-				return;
-			}
-
-			if (node->right && node->right->left)
-			{
-				ret = false;
-				return;
-			}
-			if (node->right && node->right->right)
-			{
-				ret = false;
-				return;
-			}
-		}
-
-
-		traversal(node->left);
-		traversal(node->right);
+		return std::max(left, right);
 
 	}
 
@@ -62,8 +39,6 @@ class Solution {
 
 public:
 	bool isBalanced(TreeNode* root) {
-
-		this->root = root;
 
 		traversal(root);
 
