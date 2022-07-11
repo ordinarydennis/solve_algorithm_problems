d0e54afd991e506f574ef3b8b7783960da29bb25
오준영 <ordinarydennis@gmail.com>
2022-07-11 13:15:51 +0900

    Add LeetCode Solution - 133. Clone Graph(fail)

--- leetcode_133_Clone_Graph.h
+++ leetcode_133_Clone_Graph.h
@@ -2,53 +2,55 @@
 // Definition for a Node.
 class Node {
 public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
+	int val;
+	vector<Node*> neighbors;
+	Node() {
+		val = 0;
+		neighbors = vector<Node*>();
+	}
+	Node(int _val) {
+		val = _val;
+		neighbors = vector<Node*>();
+	}
+	Node(int _val, vector<Node*> _neighbors) {
+		val = _val;
+		neighbors = _neighbors;
+	}
 };
 */
+
 class Solution {
 
-    void traversalGraph(Node* origin, Node* dest)
-    {
-        if (nullptr == origin)
-            return;
+	void traversalGraph(const Node* node, Node* dest, vector<bool>& check)
+	{
 
-        Node* newNode = nullptr;
+		for (const auto* neighbor : node->neighbors)
+		{
+			Node* newNode = new Node(neighbor->val);
 
-        for (auto* node : origin->neighbors)
-        {
-            newNode = new Node;
-            newNode->val = node->val;
-            dest->neighbors.push_back(newNode);
-        }
+			dest->neighbors.push_back(newNode);
 
-        for (int i = 0; i < dest->neighbors.size(); i++)
-        {
-            traversalGraph(origin->neighbors[i], dest->neighbors[i]);
-        }
+			if (false == check[neighbor->val])
+			{
+				check[neighbor->val] = true;
+				traversalGraph(neighbor, newNode, check);
+			}
+		}
 
-    }
+	}
 
 public:
-    Node* cloneGraph(Node* node) {
+	Node* cloneGraph(Node* node) {
 
-        Node* dest = new Node;
+		vector<bool> check(101, false);
 
-        traversalGraph(node, dest);
+		Node* dest = new Node(node->val);
 
-        return dest;
+		check[node->val] = true;
 
-    }
+		traversalGraph(node, dest, check);
+
+		return dest;
+
+	}
 };
