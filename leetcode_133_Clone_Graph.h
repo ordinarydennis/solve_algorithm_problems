1935116dcca13246d0f884678a5d5bf4aca8ba73
ordinarydennis <ordinarydennis@gmail.com>
2022-07-17 19:24:37 +0900

    Add LeetCode Solution - 133. Clone Graph - Add Solution using BFS

--- leetcode_133_Clone_Graph.h
+++ leetcode_133_Clone_Graph.h
@@ -2,66 +2,107 @@
 // Definition for a Node.
 class Node {
 public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
+	int val;
+	vector<Node*> neighbors;
+	Node() {
+		val = 0;
+		neighbors = vector<Node*>();
+	}
+	Node(int _val) {
+		val = _val;
+		neighbors = vector<Node*>();
+	}
+	Node(int _val, vector<Node*> _neighbors) {
+		val = _val;
+		neighbors = _neighbors;
+	}
 };
 */
 
+//dfs
 class Solution {
 
-    Node* traverse(Node* node, std::unordered_map<Node*, Node*>& checker)
-    {
-        Node* newNode = new Node(node->val);
+	Node* traverse(const Node* ori, std::unordered_map<const Node*, Node*>& checker)
+	{
 
-        checker.insert(
-            { node, newNode }
-        );
+		Node* newNode = new Node(ori->val);
 
-        for (Node* neighbor : node->neighbors)
-        {
-            const auto& it = checker.find(neighbor);
+		checker.emplace(ori, newNode);
 
-            Node* node = nullptr;
+		for (const auto* nNode : ori->neighbors)
+		{
+			const auto& it = checker.find(ori);
 
-            if (checker.end() != it)
-            {
-                node = it->second;
-            }
-            else
-            {
-                node = traverse(neighbor, checker);
-            }
+			if (checker.end() == it)
+			{
+				newNode->neighbors.push_back(traverse(nNode, checker));
+			}
+			else
+			{
+				newNode->neighbors.push_back(it->second);
+			}
+		}
 
-            newNode->neighbors.push_back(node);
+		return newNode;
+	}
 
-        }
-
-        return newNode;
-    }
 
 public:
-    Node* cloneGraph(Node* node) {
+	Node* cloneGraph(Node* node) {
 
-        if (nullptr == node)
-        {
-            return nullptr;
-        }
+		std::unordered_map<const Node*, Node*> checker;
 
-        std::unordered_map<Node*, Node*> checker;
+		traverse(node, checker);
 
-        return traverse(node, checker);
+	}
+};
 
-    }
+//bfs
+class Solution {
+
+public:
+	Node* cloneGraph(Node* node) {
+
+		if (nullptr == node)
+			return nullptr;
+
+		std::queue<const Node*> q;
+
+		q.push(node);
+
+		std::unordered_map<const Node*, Node*> m;
+
+		while (false == q.empty())
+		{
+			const Node* e = q.front();
+			q.pop();
+
+			Node* n = new Node(e->val);
+
+			m.emplace(e, n);
+
+			for (const Node* neighbor : e->neighbors)
+			{
+				const auto& it = m.find(neighbor);
+
+				Node* newNeighbor = nullptr;
+
+				if (m.end() == it)
+				{
+					newNeighbor = new Node(e->val);
+					q.push(neighbor);
+				}
+				else
+				{
+					newNeighbor = it->second;
+				}
+
+				n->neighbors.push_back(newNeighbor);
+			}
+
+		}
+
+		return m[node];
+
+	}
 };
