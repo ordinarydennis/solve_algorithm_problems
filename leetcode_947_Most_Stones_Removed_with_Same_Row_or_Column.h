3a6e26977a474e6da4035a8cbf93adffc0db4085
Dennis <ordinarydennis@gmail.com>
2022-07-23 12:08:36 +0900

    Edit LeetCode Solution - 947. Most Stones Removed with Same Row or Column(read solution)

--- leetcode_947_Most_Stones_Removed_with_Same_Row_or_Column.h
+++ leetcode_947_Most_Stones_Removed_with_Same_Row_or_Column.h
@@ -1,63 +1,34 @@
 class Solution {
 public:
-	int removeStones(vector<vector<int>>& stones) {
+    int dfs(unordered_map<int, vector<int>>& rows, unordered_map<int, vector<int>>& cols, unordered_set<int>& v_rows, int row) {
+        
+        auto res = 0;
+        
+        if (v_rows.insert(row).second)
+        {
+            res += rows[row].size();
 
-		//좌표가 정렬이 안되어 있다면 정렬하자
-		int ret = 0;
+            for (auto c : rows[row])
+            {
+                for (auto r : cols[c]) res += dfs(rows, cols, v_rows, r);
 
-		vector<vector<int>> plane(1000, vector<int>(1000, 0));
-
-		int maxColumn = 1000;
-		int maxRow = 1000;
-
-		for (const auto& stonePoint : stones)
-		{
-			plane[stonePoint[1]][stonePoint[0]] = 1;
-		}
-
-		for (int i = stones.size() - 1; 0 <= i; i--)
-		{
-			bool found = false;
-
-			const auto& stonePoint = stones[i];
-
-			int x = stonePoint[0];
-
-			for (int i = 0; i < maxRow; i++)
-			{
-				if (plane[i][x])
-				{
-					found = true;
-					break;
-				}
-			}
-
-			if (found)
-			{
-				ret++;
-				continue;
-			}
-
-			int y = stonePoint[1];
-
-			for (int i = 0; i < maxColumn; i++)
-			{
-				if (plane[y][i])
-				{
-
-					found = true;
-					break;
-				}
-			}
-
-			if (found)
-			{
-				ret++;
-				continue;
-			}
-		}
-
-		return ret;
-
-	}
+            }
+        }
+        
+        return res;
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        
+        unordered_map<int, vector<int>> rows, cols;
+        
+        unordered_set<int> v_rows;
+        
+        auto res = 0;
+        
+        for (auto s : stones) rows[s[0]].push_back(s[1]), cols[s[1]].push_back(s[0]);
+        
+        for (auto r : rows) res += max(0, dfs(rows, cols, v_rows, r.first) - 1);
+        
+        return res;
+    }
 };
