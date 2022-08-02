b04775a99b6e15b7e6a63423987eae83d72651e8
오준영 <ordinarydennis@gmail.com>
2022-08-02 22:47:16 +0900

    Add LeetCode Solution - 2357. Make Array Zero by Subtracting Equal Amounts(fail)

---
+++ leetcode_357_Make_Array_Zero_by_Subtracting_Equal_Amounts.h
@@ -0,0 +1,31 @@
+class Solution {
+public:
+	int minimumOperations(vector<int>& nums) {
+
+		std::set<int> set;
+
+		for (const int n : nums)
+		{
+			if (0 != n)
+			{
+				set.insert(n);
+			}
+			
+		}
+
+		while (set.size())
+		{
+			const auto& it = set.begin();
+
+			*it;
+			
+
+			for ()
+			{
+
+			}
+
+				
+		}
+	}
+};
