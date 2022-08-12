28c91acb0533800b4ef5511ed138250272c2cb6f
Dennis <ordinarydennis@gmail.com>
2022-08-12 23:46:27 +0900

    Edit LeetCode Solution - 67. Add Binary(read solution)

--- leetcode_67_Add_Binary.h
+++ leetcode_67_Add_Binary.h
@@ -1,46 +1,19 @@
-class Solution {
+class Solution
+{
 public:
-    string addBinary(string a, string b) {
+    string addBinary(string a, string b)
+    {
+        string s = "";
 
-        reverse(a.begin(), a.end());
-        reverse(b.begin(), b.end());
-
-        int minSize = std::min(a.size(), b.size());
-
-        std::string ret = a.size() < b.size() ? b : a;
-
-        int plus = 0;
-
-        for (int i = 0; i < minSize; i++)
+        int c = 0, i = a.size() - 1, j = b.size() - 1;
+        while (i >= 0 || j >= 0 || c == 1)
         {
-            int num1 = (a[i] == '0') ? 0 : 1;
-            int num2 = (b[i] == '0') ? 0 : 1;
-
-            int sum = num1 + num2 + plus;
-
-            if (2 <= sum)
-            {
-                ret[i] = (2 == sum) ? '0' : '1';
-                plus = 1;
-            }
-            else if (num1 == 0 && num2 == 0)
-            {
-                ret[i] = '0';
-            }
-            else
-            {
-                ret[i] = '1';
-            }
+            c += i >= 0 ? a[i--] - '0' : 0;
+            c += j >= 0 ? b[j--] - '0' : 0;
+            s = char(c % 2 + '0') + s;
+            c /= 2;
         }
 
-        if (a.size() != b.size() && 1 == plus)
-        {
-            ret += '1';
-        }
-
-        reverse(ret.begin(), ret.end());
-
-        return ret;
-
+        return s;
     }
 };
