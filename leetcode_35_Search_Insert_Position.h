77b0b6a9cc0aaaa94cf4f4af098c26f253065e4d
Dennis <ordinarydennis@gmail.com>
2022-08-13 22:25:02 +0900

    Edit LeetCode Solution - 35. Search Insert Position(read solution)

--- leetcode_35_Search_Insert_Position.h
+++ leetcode_35_Search_Insert_Position.h
@@ -5,69 +5,21 @@ public:
 		int start = 0;
 		int end = nums.size() - 1;
 
-		int ret = 0;
-
-		while (1)
+		while (start <= end)
 		{
-			if (end - start == 1)
-			{
-				if (nums[start] == target)
-				{
-					ret = start;
-				}
-				else if (nums[start] < target && target < nums[end])
-				{
-					ret = start + 1;
-				}
-				else if (nums[end] == target)
-				{
-					ret = end;
-				}
-				else if (nums[end] < target)
-				{
-					ret = end + 1;
-				}
-				break;
-			}
-			else if (end == start)
-			{
-				if (nums[start] == target)
-				{
-					ret = start;
-				}
-				else if (nums[start] < target)
-				{
-					ret = start + 1;
-				}
-				else
-				{
-					ret = start - 1;
-				}
-				break;
-			}
-
 			int mid = (start + end) / 2;
 
-			if (target == nums[mid])
-			{
-				ret = mid;
-				break;
-			}
-			else if (target < nums[mid])
-			{
-				end = mid - 1;
-			}
-			else
+			if (nums[mid] < target)
 			{
 				start = mid + 1;
 			}
+			else
+			{
+				end = mid - 1;
+			}
 		}
 
-		if (ret < 0)
-		{
-			ret = 0;
-		}
+		return start;
 
-		return ret;
 	}
 };
