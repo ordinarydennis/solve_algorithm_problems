0bfa67dceb41c8c2b4df47bcad96d4db97dc50b4
Dennis <ordinarydennis@gmail.com>
2022-08-21 23:10:48 +0900

    Edit LeetCode Solution - 171. Excel Sheet Column Number(solve again)

--- leetcode_171_Excel_Sheet_Column_Number.h
+++ leetcode_171_Excel_Sheet_Column_Number.h
@@ -2,20 +2,24 @@ class Solution {
 public:
 	int titleToNumber(string columnTitle) {
 
-		int gap = 'Z' - 'A';
-		int pos = 0;
-		int total = 0;
+		int count = 0;
+
+		int sum = 0;
 
 		for (int i = columnTitle.size() - 1; 0 <= i; i--)
 		{
 			int num = columnTitle[i] - 'A' + 1;
 
-			total += pos * gap * num + num;
+			for(int a = 0; a < count; a++)
+			{
+				num *= 26;
+			}
 
-			pos++;
+			sum += num;
+			count++;
 		}
 
-		return total;
+		return sum;
 
 	}
 };
