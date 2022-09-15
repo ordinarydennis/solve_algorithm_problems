6a24086e250ee56d01dc1b932bc40d9a93e3ec4e
오준영 <ordinarydennis@gmail.com>
2022-09-15 12:49:04 +0900

    Add LeetCode Solution - 520. Detect Capital

---
+++ leetcode_520_Detect_Capital.h
@@ -0,0 +1,40 @@
+class Solution {
+
+	bool isCapitals(char c)
+	{
+		if ('A' <= c && c <= 'Z')
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+
+public:
+	bool detectCapitalUse(string word) {
+
+		int capCount = 0;
+
+		for (int i = 0; i< word.size(); i++)
+		{ 
+			if (isCapitals(word[i]))
+			{
+				capCount++;
+			}
+		}
+
+		if (capCount == 0 || capCount == word.size())
+		{
+			return true;
+		}
+
+		if (1 == capCount && isCapitals(word[0]))
+		{
+			return true;
+			
+			
+		}
+
+	}
+};
