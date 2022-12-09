dce4e7b13703b1403159cd9de690eb0b985e0a2d
오준영 <ordinarydennis@gmail.com>
2022-12-09 13:22:33 +0900

    Add LeetCode Solution - 1945. Sum of Digits of String After Convert(25m)

---
+++ leetcode_1945_Sum_of_Digits_of_String_After_Convert.h
@@ -0,0 +1,35 @@
+﻿class Solution {
+public:
+	int getLucky(string s, int k) {
+
+		int sum = 0;
+
+		for (int i = 0; i < s.size(); i++)
+		{
+			int n = s[i] - 'a' + 1;
+
+			if (10 <= n)
+			{
+				sum += n / 10;
+				sum += n % 10;
+			}
+			else
+			{
+				sum += n;
+			}
+		}
+
+		for (int i = 1; i < k; i++)
+		{
+			int ret = 0;
+			while (0 < sum)
+			{
+				ret += sum % 10;
+				sum /= 10;
+			}
+			sum = ret;
+		}
+
+		return sum;
+	}
+};
